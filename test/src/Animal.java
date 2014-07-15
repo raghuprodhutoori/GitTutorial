75dfa1d1d87f17e628157c3d8cef230bf79ecffc
raghuprodhutoori <raghu.prodhutoori@gmail.com>
2014-07-15 01:33:21 -0500

    Initial

---
+++ test/src/Animal.java
@@ -0,0 +1,30 @@
+
+public class Animal {
+
+	public void move(){
+	      System.out.println("Animals can move");
+	   }
+	}
+
+	class Dog extends Animal{
+
+	   public void move(){
+	      System.out.println("Dogs can walk and run");
+	   }
+	   public void bark(){
+	      System.out.println("Dogs can bark");
+	   }
+	}
+
+	class TestDog{
+
+	   public static void main(String args[]){
+	      Animal a = new Animal(); // Animal reference and object
+	      Animal b = new Dog(); // Animal reference but Dog object
+
+	      a.move();// runs the method in Animal class
+	      b.move();//Runs the method in Dog class
+	      Dog d = (Dog) b;
+	      d.bark();
+	   }
+}
